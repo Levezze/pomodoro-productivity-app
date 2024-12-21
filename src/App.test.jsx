45b2c4305f58dcab388cbac50cf4f7f4373f8678
Levezze <lev.zhi@gmail.com>
2024-12-21 14:37:04 -0500

    First commit

---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { describe } from "vitest";
+
+describe('App', () => {
+  it('renders the App component', () => {
+    render( <App /> )
+
+    screen.debug(); // prints the jsx in the component
+  })
+})
