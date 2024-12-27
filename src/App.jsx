2424631e77d24242900590c471dd898889331551
Levezze <lev.zhi@gmail.com>
2024-12-27 02:19:02 -0500

    Add break time, auto-switch to break countdown after work timer reaches 0

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Button } from './ui/Button'
 import './App.css'
 import { Timer } from './components/Timer'
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false)
-  const displayLogin = (
+
+  const loginButton = (
     <div className='login'>
       <Button 
         buttonText={"Login"} 
-        handleClick={undefined} 
+        handleClick={() => setLoggedIn(true)} 
       />
     </div>
-    );
-  const displayStart = (
+  );
+
+  const startButton = (
     <div className='start-session'>
         <Button 
           buttonText={"Start Session"} 
@@ -23,23 +23,11 @@ function App() {
         />
       </div>
   );
-  
+
   return (
     <main>
       <section className='login-start'>
-        {loggedIn ? displayStart : displayLogin}
-        {/* <div className='login'>
-          <Button 
-            buttonText={"Login"} 
-            handleClick={undefined} 
-          />
-        </div>
-        <div className='start-session'>
-          <Button 
-            buttonText={"Start Session"} 
-            handleClick={undefined} 
-          />
-        </div> */}
+        {loggedIn ? startButton : loginButton}
       </section>
       <section className='container pomodoro'>
         <Timer />
--- src/forms/SetTime.jsx
+++ src/forms/SetTime.jsx
@@ -1,21 +1,29 @@
 import React, {useState} from "react";
 import { timeInMilisec } from "../utils/timeInMils";
 
-export function SetTime({ timeOption, setTimeOption, setTimeLeft, defaultTime }) {  
+export function SetTime({ timeOption, setTimeOption, setTimeLeft, defaultTime, defaultBreak, setBreakTime }) {  
   const handleChange = (e) => {
     setTimeOption(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e, stateSetter) => {
     e.preventDefault()
-    setTimeLeft(timeInMilisec(timeOption));
+    stateSetter(timeInMilisec(timeOption));
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="pomodoroSetTime"></label>
-      <input id="pomodoroSetTime" type="number" onChange={handleChange} defaultValue={defaultTime} min="0" />
-      <button htmlFor="pomodoroSetTime" type="submit">Set Time</button>
-    </form>
+    <>
+      <form id="setTimeForm" onSubmit={(e) => handleSubmit(e, setTimeLeft)}>
+        <label htmlFor="pomodoroSetTime"></label>
+        <input id="pomodoroSetTime" type="number" onChange={handleChange} defaultValue={defaultTime} min="1" />
+        <button htmlFor="pomodoroSetTime" type="submit">Set Time</button>
+      </form>
+      <form id="setBreakForm" onSubmit={(e) => handleSubmit(e, setBreakTime)}>
+        <label htmlFor="pomodoroSetBreak"></label>
+        <input id="pomodoroSetBreak" type="number" onChange={handleChange} defaultValue={defaultBreak} min="1" />
+        <button htmlFor="pomodoroSetTime" type="submit">Set Break</button>
+      </form>
+    </>
+    
   )
 }
